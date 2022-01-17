b97c5467e3192de6cdbfda25f06879b790daf906
Mathieu BERGERON <E210826J@univ-nantes.fr>
2022-01-17 09:45:38 +0100

    version finale (normalement)

--- SAEparticule/particles/update.go
+++ SAEparticule/particles/update.go
@@ -18,29 +18,28 @@ func (s *System) Update() {
       s.Content[p].SpeedX = -s.Content[p].SpeedX
     }
 
-    if s.Content[p].PositionX >= float64(config.General.WindowSizeX){
-      s.Content = remove(s.Content, p)
-      break
-    }
-    if s.Content[p].PositionX <= -50{
-      s.Content = remove(s.Content, p)
-      break
-    }
     if !(s.Content[p].PositionY + float64(config.General.ImgSizeY)*s.Content[p].ScaleY >= float64(config.General.WindowSizeY)){
       s.Content[p].PositionY += s.Content[p].SpeedY
       s.Content[p].PositionX += s.Content[p].SpeedX
-      r := rand.Float64()*rand.Float64()/30000
-      s.Content[p].ScaleX -= r
-      s.Content[p].ScaleY -= r
-    }else{
-      r := rand.Float64()*rand.Float64()/30000
-      s.Content[p].ScaleX -= 3*r
-      s.Content[p].ScaleY -= 3*r
     }
+    r := rand.Float64()*rand.Float64()/30000
+    s.Content[p].ScaleX -= r
+    s.Content[p].ScaleY -= r
 
     if s.Content[p].ScaleX <= 0{
-      s.Content = remove(s.Content, p)
-      break
+      s.Spawnrate -= 1
+      rand.Seed(time.Now().UnixNano())
+      spdX := rand.Float64()
+      spdX -= 0.5
+      scale := rand.Float64()*0.01
+      s.Content[p] = Particle{
+        PositionX: rand.Float64()*float64(config.General.WindowSizeX),
+        PositionY: -50,
+        ScaleX:	scale, ScaleY: scale,
+        ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+        Opacity: 1,
+        SpeedX: spdX * 2, SpeedY: 2,
+      }
     }
   }
 
@@ -50,9 +49,9 @@ func (s *System) Update() {
     rand.Seed(time.Now().UnixNano())
     spdX := rand.Float64()
     spdX -= 0.5
-    scale := rand.Float64()*0.04
+    scale := rand.Float64()*0.01
     s.Content = append(s.Content, Particle{
-      PositionX: rand.Float64()*float64(config.General.WindowSizeX),
+      PositionX: rand.Float64()*(2*float64(config.General.WindowSizeX))-float64(config.General.WindowSizeX),
       PositionY: -50,
       ScaleX:	scale, ScaleY: scale,
       ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
@@ -61,19 +60,4 @@ func (s *System) Update() {
     })
   }
   log.Print(len(s.Content))
-}
-
-func remove(t []Particle, s int) (l []Particle){
-  if s > len(t)/2{
-    l = t[s+1:]
-    l = append(l, t[:s]...)
-  }else{
-    l = t[:s]
-    l = append(l, t[s+1:]...)
-  }
-  return l
-}
-
-func collision(t []Particle, s int) int{
-  return 0
 }
